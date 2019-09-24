239e7c4c58b75cd21cca6a106f251fed5bc02a06
Sanggyu Nam <pokeplus@gmail.com>
2019-09-25 01:31:24 +0900

    Solve exercise 5.15

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -103,6 +103,11 @@ trait Stream[+A] {
     case (Some(za), Some(zb)) if (za == zb) => true
     case _ => false
   })
+
+  def tails: Stream[Stream[A]] = unfold(this)(s => s match {
+    case Cons(_, t) => Some(s, t())
+    case _ => None
+  })
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
