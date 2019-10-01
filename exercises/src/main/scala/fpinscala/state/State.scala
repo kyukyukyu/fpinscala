ad98e3b02358409946a4a01137c8e6ae1b4fdfa8
Sanggyu Nam <pokeplus@gmail.com>
2019-10-02 01:06:06 +0900

    Solve exercise 6.10

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -104,11 +104,13 @@ object RNG {
 
 case class State[S,+A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
-    ???
+    flatMap(a => State.unit(f(a)))
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    flatMap(a => sb.map(b => f(a, b)))
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s1 => {
+    val (a, s2) = run(s1)
+    f(a).run(s2)
+  })
 }
 
 sealed trait Input
@@ -119,5 +121,8 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
+  def unit[S,A](a: A): State[S, A] = State(s => (a, s))
+  def sequence[S,A](fs: List[State[S, A]]): State[S, List[A]] =
+    fs.foldRight(unit[S,List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }
